590e99e230e708cca91274d0cddacb96fc75007c
palomagl <palomalorenzon1993@gmail.com>
2025-06-08 11:25:37 -0300

    Remove pastas desafio2, desafio3 e desafio4 do repositório mantendo local

--- desafio2/app.js
+++
@@ -1,9 +0,0 @@
-function clicarConsole(n1,n2) {
-    if (n1 > n2) {
-        console.log(n1);
-    } else {
-        console.log(n2);
-    }
-}
-
-clicarConsole(80,200);
--- desafio4/app.js
+++
@@ -1,7 +0,0 @@
-/*let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
-
-linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
-console.log(linguagensDeProgramacao); */
-
-let lista = ["paloma", "yy", "xx"];
-console.log(lista);
